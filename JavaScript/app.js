f8576c7ea3bd603a2c4cd591503de33e7918b2fc
Elvin <elvinsrkrov@gmail.com>
2023-10-09 09:58:24 +0400

    added addTaskForm , addBtn button and then add click event to the btn for the toggle addTaskForm

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,10 +1,21 @@
 const wrapper = document.querySelector(".wrapper");
 const menuBtn = document.querySelector(".menu-btn");
 const backBtn = document.querySelector(".back-btn");
-
+const addTaskBtn = document.querySelector(".add-task-btn");
+const addTaskForm = document.querySelector(".add-task");
+const blackBackDrop = document.querySelector(".black-backdrop");
 
 const toggleScreen = () => {
-    wrapper.classList.toggle("show-category");
+  wrapper.classList.toggle("show-category");
 };
-backBtn.addEventListener("click", toggleScreen)
+
+const toggleAddTaskForm = () => {
+  addTaskForm.classList.toggle("active");
+  blackBackDrop.classList.toggle("active");
+  addTaskBtn.classList.toggle("active");
+};
+
+addTaskBtn.addEventListener("click", toggleAddTaskForm);
+blackBackDrop.addEventListener("click", toggleAddTaskForm);
+backBtn.addEventListener("click", toggleScreen);
 menuBtn.addEventListener("click", toggleScreen);

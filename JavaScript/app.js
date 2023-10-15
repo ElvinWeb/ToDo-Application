785786f47115b6745b37642b178dc56a99599e20
Elvin <elvinsrkrov@gmail.com>
2023-10-15 13:08:28 +0400

    rendering and create category cards with categoryies object

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -19,3 +19,263 @@ addTaskBtn.addEventListener("click", toggleAddTaskForm);
 blackBackDrop.addEventListener("click", toggleAddTaskForm);
 backBtn.addEventListener("click", toggleScreen);
 menuBtn.addEventListener("click", toggleScreen);
+
+//adding the category and tasks
+let categories = [
+  {
+    title: "Personal",
+    img: "boy.png",
+  },
+  {
+    title: "Work",
+    img: "briefcase.png",
+  },
+  {
+    title: "Shopping",
+    img: "shopping.png",
+  },
+  {
+    title: "Coding",
+    img: "web-design.png",
+  },
+  {
+    title: "Health",
+    img: "healthcare.png",
+  },
+  {
+    title: "Fitness",
+    img: "dumbbell.png",
+  },
+  {
+    title: "Education",
+    img: "education.png",
+  },
+  {
+    title: "Finance",
+    img: "saving.png",
+  },
+];
+let tasks = [
+  {
+    id: 1,
+    task: "Go to market",
+    category: "Shopping",
+    completed: false,
+  },
+  {
+    id: 2,
+    task: "Read a chapter of a book",
+    category: "Personal",
+    completed: false,
+  },
+  {
+    id: 3,
+    task: "Prepare presentation for meeting",
+    category: "Work",
+    completed: false,
+  },
+  {
+    id: 4,
+    task: "Complete coding challenge",
+    category: "Coding",
+    completed: false,
+  },
+  {
+    id: 5,
+    task: "Take a 30-minute walk",
+    category: "Health",
+    completed: false,
+  },
+  {
+    id: 6,
+    task: "Do a 20-minute HIIT workout",
+    category: "Fitness",
+    completed: false,
+  },
+  {
+    id: 7,
+    task: "Watch an educational video online",
+    category: "Education",
+    completed: false,
+  },
+  {
+    id: 8,
+    task: "Review monthly budget",
+    category: "Finance",
+    completed: false,
+  },
+  {
+    id: 9,
+    task: "Buy groceries for the week",
+    category: "Shopping",
+    completed: false,
+  },
+  {
+    id: 10,
+    task: "Write in a journal",
+    category: "Personal",
+    completed: false,
+  },
+  {
+    id: 11,
+    task: "Send follow-up emails",
+    category: "Work",
+    completed: false,
+  },
+  {
+    id: 12,
+    task: "Work on a coding side project",
+    category: "Coding",
+    completed: false,
+  },
+  {
+    id: 13,
+    task: "Try a new healthy recipe",
+    category: "Health",
+    completed: false,
+  },
+  {
+    id: 14,
+    task: "Attend a yoga class",
+    category: "Fitness",
+    completed: false,
+  },
+  {
+    id: 15,
+    task: "Read an article about a new topic",
+    category: "Education",
+    completed: false,
+  },
+  {
+    id: 16,
+    task: "Set up automatic bill payments",
+    category: "Finance",
+    completed: false,
+  },
+  // Additional tasks for each category
+  {
+    id: 17,
+    task: "Buy new clothes",
+    category: "Shopping",
+    completed: false,
+  },
+  {
+    id: 18,
+    task: "Meditate for 10 minutes",
+    category: "Personal",
+    completed: false,
+  },
+  {
+    id: 19,
+    task: "Prepare agenda for team meeting",
+    category: "Work",
+    completed: false,
+  },
+  {
+    id: 20,
+    task: "Debug a software issue",
+    category: "Coding",
+    completed: false,
+  },
+  {
+    id: 21,
+    task: "Try a new recipe for lunch",
+    category: "Health",
+    completed: false,
+  },
+  {
+    id: 22,
+    task: "Go for a run",
+    category: "Fitness",
+    completed: false,
+  },
+  {
+    id: 23,
+    task: "Learn a new language online",
+    category: "Education",
+    completed: false,
+  },
+  {
+    id: 24,
+    task: "Read about history",
+    category: "Education",
+    completed: false,
+  },
+  {
+    id: 25,
+    task: "Review investment portfolio",
+    category: "Finance",
+    completed: false,
+  },
+  // Add more tasks for each category as desired
+];
+
+let selectedCategory = categories[0];
+
+const categoriesContainer = document.querySelector(".categories");
+const categoryTitle = document.querySelector(".category-title");
+const totalCategoryTasks = document.querySelector(".category-tasks");
+const categoryImg = document.getElementById("category-img");
+const totalTasks = document.querySelector(".totalTasks");
+
+//calculate the total category tasks
+const calculateTotal = () => {
+  const categoryTasks = tasks.filter(
+    (task) =>
+      task.category.toLowerCase() === selectedCategory.title.toLowerCase()
+  );
+  totalCategoryTasks.textContent = `${categoryTasks.length} Tasks`;
+};
+
+//rendering the category cards
+const renderCategories = () => {
+  categoriesContainer.innerHTML = "";
+  totalTasks.textContent = tasks.length;
+
+  categories.forEach((category) => {
+    const categoryTasks = tasks.filter(
+      (task) => task.category.toLowerCase() == category.title.toLowerCase()
+    );
+
+    //create a category card div
+    const div = document.createElement("div");
+    div.classList.add("category");
+    div.addEventListener("click", () => {
+      wrapper.classList.add("show-category");
+      selectedCategory = category;
+      categoryImg.src = `../Images/${category.img}`;
+      categoryTitle.textContent = category.title;
+      calculateTotal();
+    });
+    div.innerHTML = ` 
+    <div class="left">
+      <img src="../Images/${category.img}" alt=${category.title} />
+      <div class="content">
+        <h1>${category.title}</h1>
+        <p>${categoryTasks.length} Tasks</p>
+      </div>
+    </div>
+    <div class="options">
+      <div class="toggle-btn">
+          <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+          >
+          <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+          />
+          </svg>
+      </div>
+    </div>
+    `;
+    categoriesContainer.appendChild(div);
+  });
+};
+
+renderCategories();
